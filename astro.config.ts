5678c29f80f8ee5ca465daba2678d7a670209f0b
godsenal <tmqps78@gmail.com>
2023-01-29 16:44:32 +0900

    :sparkles: autolink 추가

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,24 @@ import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import codeTitlePlugin from "rehype-code-titles";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { customPlugin } from "./customRemarkPlugin";
 
 export default defineConfig({
   markdown: {
-    remarkPlugins: [codeTitlePlugin, customPlugin],
+    remarkPlugins: [customPlugin],
+    // https://docs.astro.build/en/guides/markdown-content/#heading-ids-and-plugins
+    rehypePlugins: [
+      rehypeHeadingIds,
+      codeTitlePlugin,
+      () =>
+        rehypeAutolinkHeadings({
+          properties: {
+            className: ["heading-link"],
+          },
+        }),
+    ],
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true,
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,11 +10,22 @@ module.exports = {
       {
         light: {
           ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#444494",
         },
       },
       {
         dark: {
-          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          primary: "#cbc3e3",
+          secondary: "#f39eff",
+          accent: "#1FB2A5",
+          neutral: "#191D24",
+          "base-100": "#222831",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+          // "base-content": "#cbd4d9",
+          "primary-content": "#222831",
         },
       },
     ],
