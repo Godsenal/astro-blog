728a916f5ce7ffe542ea7a07aadcada6ac4e7d45
godsenal <tmqps78@gmail.com>
2023-01-31 23:23:01 +0900

    :sparkles: frontmatter 적용

--- customRemarkPlugin.ts
+++ customRemarkPlugin.ts
@@ -3,14 +3,13 @@ import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
 
 const EXCERPT_SEPARATOR = "<!--more-->";
+const PRUNE_LENGTH = 140;
 
 export const customPlugin: RemarkPlugin = () => {
   return async (tree, { data }) => {
     const plain = toString(tree);
     const readingTime = getReadingTime(plain);
-    const excerpt = plain.includes(EXCERPT_SEPARATOR)
-      ? plain.split(EXCERPT_SEPARATOR)[0]
-      : "";
+    const excerpt = plain.split(EXCERPT_SEPARATOR)[0]?.slice(0, PRUNE_LENGTH);
 
     const astroData = data.astro as MarkdownAstroData;
 
