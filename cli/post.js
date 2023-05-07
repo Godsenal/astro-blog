60d21668901f83c235abdb4604c2f707a01af589
godsenal <tmqps78@gmail.com>
2023-05-08 01:04:27 +0900

    :wrench: post 생성 스크립트 변경된 프로젝트에 맞춰 수정

--- cli/post.js
+++ cli/post.js
@@ -1,38 +1,34 @@
-const fs = require('fs');
-const path = require('path');
-const inquirer = require('inquirer');
-const matter = require('gray-matter');
-const chalk = require('chalk');
-const ora = require('ora');
-const format = require('date-fns/format');
-
-const omitBy = require('lodash/omitBy');
-const isEmpty = require('lodash/isEmpty');
-const diff = require('lodash/difference');
-const uniq = require('lodash/uniq');
+import { existsSync, mkdirSync } from "fs";
+import { writeFile } from "fs/promises";
+import { resolve } from "path";
+import inquirer from "inquirer";
+import autocomplete from "inquirer-autocomplete-prompt";
+import grayMatter from "gray-matter";
+import chalk from "chalk";
+import ora from "ora";
+import { format } from "date-fns";
+import { omitBy, isEmpty, difference, uniq } from "lodash-es";
+import { glob } from "glob";
+import { exec } from "child_process";
 
 const log = console.log;
 
-const { promisify } = require('util');
-inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
+inquirer.registerPrompt("autocomplete", autocomplete);
 
-const glob = promisify(require('glob'));
-const writeFile = promisify(fs.writeFile);
-
-const postsPath = path.resolve(process.cwd(), 'src', 'pages', 'posts');
+const postsPath = resolve(process.cwd(), "src", "content", "post");
 
 const readPosts = () => glob(`${postsPath}/**/index.md`);
 const handleTitle = async (titleList) => {
   const { title } = await inquirer.prompt({
-    name: 'title',
-    message: 'What is your post title?',
+    name: "title",
+    message: "What is your post title?",
     validate: (val) => {
       return new Promise((res) => {
         if (!val || val.length < 2) {
-          return res('Title must be 0 ~ 2 length.');
+          return res("Title must be 0 ~ 2 length.");
         }
         if (titleList.some((item) => item === val)) {
-          return res('Title is already exist!');
+          return res("Title is already exist!");
         }
         return res(true);
       });
@@ -43,23 +39,23 @@ const handleTitle = async (titleList) => {
 const handleCategories = async (choices) => {
   const createIdentity = -1;
   let result = await inquirer.prompt({
-    type: 'list',
-    name: 'categories',
-    message: 'Select categories.',
+    type: "list",
+    name: "categories",
+    message: "Select categories.",
     choices: [
-      { name: 'Create other category.', value: createIdentity },
+      { name: "Create other category.", value: createIdentity },
       ...choices,
       { name: "I don't want to have category...", value: null },
     ],
   });
   if (result.categories === createIdentity) {
     result = await inquirer.prompt({
-      name: 'categories',
-      message: 'Type new category.',
+      name: "categories",
+      message: "Type new category.",
       validate: (val) => {
         return new Promise((res) => {
           if (!val || val.length < 2) {
-            return res('Category must be 0 ~ 2 length.');
+            return res("Category must be 0 ~ 2 length.");
           }
           return res(true);
         });
@@ -70,20 +66,24 @@ const handleCategories = async (choices) => {
 };
 const handleTags = async (choices, results = []) => {
   const FINISH = [{ name: `OK. DONE!`, value: null }];
-  const currChoices = [...diff(choices, results)];
+  const currChoices = [...difference(choices, results)];
   const { result } = await inquirer.prompt({
-    type: 'autocomplete',
-    name: 'result',
-    message: 'Select a tag.',
+    type: "autocomplete",
+    name: "result",
+    message: "Select a tag.",
     source: (curr, input) => {
       return new Promise((res) => {
         if (!input) {
           return res(FINISH.concat(currChoices));
         }
-        const searchResult = currChoices.filter((choice) => new RegExp(`^${input}`).test(choice));
+        const searchResult = currChoices.filter((choice) =>
+          new RegExp(`^${input}`).test(choice)
+        );
         // Resolve search results or create Tag
         return res(
-          searchResult.length ? searchResult : [{ name: `create ${input} tag.`, value: input }],
+          searchResult.length
+            ? searchResult
+            : [{ name: `create ${input} tag.`, value: input }]
         );
       });
     },
@@ -95,12 +95,12 @@ const handleTags = async (choices, results = []) => {
   return handleTags(choices, results.concat(result));
 };
 const confirm = async (matters) => {
-  log(chalk.magenta('Here is your post...'));
+  log(chalk.magenta("Here is your post..."));
   log(chalk.italic(matters));
   const { proceed } = await inquirer.prompt({
-    type: 'confirm',
-    name: 'proceed',
-    message: 'Do you want to proceed?',
+    type: "confirm",
+    name: "proceed",
+    message: "Do you want to proceed?",
   });
   return proceed;
 };
@@ -111,7 +111,7 @@ const fetchMatters = (posts) => {
   posts.forEach((file) => {
     const {
       data: { title, categories, tags },
-    } = matter.read(file);
+    } = grayMatter.read(file);
     title && titleSet.add(title);
     categories && categorySet.add(...categories);
     tags && tagSet.add(...tags);
@@ -119,14 +119,14 @@ const fetchMatters = (posts) => {
   return [Array.from(titleSet), Array.from(categorySet), Array.from(tagSet)];
 };
 const createMatters = (matters) => {
-  return matter.stringify('', matters);
+  return grayMatter.stringify("", matters);
 };
 const createPost = async (title, matters) => {
-  const titleWithBar = title.replace(/ /gi, '-');
+  const titleWithBar = title.replace(/ /gi, "-");
   const directoryPath = `${postsPath}/${titleWithBar}`;
 
-  if (!fs.existsSync(directoryPath)) {
-    fs.mkdirSync(directoryPath);
+  if (!existsSync(directoryPath)) {
+    mkdirSync(directoryPath);
   }
 
   const postPath = `${directoryPath}/index.md`;
@@ -135,27 +135,29 @@ const createPost = async (title, matters) => {
 };
 
 const startCommand = async (titleList, categoryList, tagList) => {
-  const date = format(new Date(), 'yyyy-MM-dd');
+  const date = format(new Date(), "yyyy-MM-dd");
   log(chalk.magenta(`Today is : ${date}`));
   const title = await handleTitle(titleList);
   const categories = await handleCategories(categoryList);
   const tags = await handleTags(tagList);
-  const matters = createMatters(omitBy({ title, date, tags, categories }, isEmpty));
+  const matters = createMatters(
+    omitBy({ title, date, tags, categories }, isEmpty)
+  );
   const proceed = await confirm(matters);
 
   if (proceed) {
-    const spinner = ora('Creating post...').start();
+    const spinner = ora("Creating post...").start();
     const createdPath = await createPost(title, matters);
-    spinner.succeed('Done!');
+    spinner.succeed("Done!");
     log(chalk.green(`You can find created post ${chalk.blue(createdPath)}`));
-    require('child_process').exec(`code ${createdPath}`);
+    exec(`code ${createdPath}`);
 
     return true;
   }
   return false;
 };
-module.exports = (async () => {
-  const spinner = ora('Fetching exist matters...').start();
+export default (async () => {
+  const spinner = ora("Fetching exist matters...").start();
   const posts = await readPosts();
   const [titleList, categoryList, tagList] = fetchMatters(posts);
   spinner.succeed(`Let's write!`);
